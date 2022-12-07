bfcb36e9a88b6c226a2c6d3b25a7c00bbfe58fc3
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2022-12-07 07:16:03 +0300

    primary layout and API connected

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express")
+const axios = require("axios")
+const PORT = 8000
+const app = express()
+const cors = require("cors")
+require('dotenv').config({path: './config/.env'})
+
+
+
+app.use(cors())
+
+
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+const token = process.env.token
+const baseUrl = process.env.baseUrl
+const url = `${baseUrl}/standings`
+
+
+app.get("/", (req , res) => {
+    axios.get(url,{
+        headers: {
+            "X-Auth-Token" : token
+        }
+    })
+    .then((response) => {
+        console.log(response.data)
+    })
+    res.render("index.ejs")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT , () => {
+    console.log("server is running ")
+})
