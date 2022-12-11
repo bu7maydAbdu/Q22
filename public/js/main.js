6eb69e527f7715ebb42014f30ed34818b67ecaf1
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2022-12-11 09:43:35 +0300

    sidebar

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,36 @@
-import { getMain } from '../../controllers/mainController'
 
-getMain()
 
+
+const burgerMenu = document.querySelector(".fa-bars")
+burgerMenu.addEventListener("click", openMenu)
+const closeMenuBtn = document.querySelector(".fa-xmark")
+const sideBar = document.querySelector(".sideBar")
+
+closeMenuBtn.addEventListener("click", closeMenu)
+
+
+function openMenu(){
+
+    if(sideBar.style.display == 'none'){
+        sideBar.style.display = 'block';
+   }
+   else {
+        sideBar.style.display = 'none';
+   }
+}
+
+
+
+function closeMenu(){
+
+    if(sideBar.style.display == 'block'){
+        sideBar.style.display = 'none';
+   }
+   else {
+        sideBar.style.display = 'block';
+   }
+
+}
 
 
 
